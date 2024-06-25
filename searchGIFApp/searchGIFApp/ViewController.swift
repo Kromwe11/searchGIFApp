1d2d2de8740250e86919345b20b146c9dd91211b
Kromwe11 <kr0mwell159@gmail.com>
2024-06-26 03:44:59 +0500

    init main

---
+++ searchGIFApp/searchGIFApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  searchGIFApp
+//
+//  Created by Висент Щепетков on 26.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
